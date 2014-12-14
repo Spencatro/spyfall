171ebee7bb1ff7d3ba16c9a4ebda66fb38e828f4
Spencer Hawkins <hawkins.spencer@gmail.com>
2014-12-13 20:43:04 -0600

    Adding simple add game function

--- wsgi/__init__.py
+++ wsgi/__init__.py
@@ -18,11 +18,17 @@ class SpyfallApp(Flask):
         super(SpyfallApp, self).__init__(arg)
         self.route("/")(self.index)
         self.route("/debug/<command>")(self.debug)
+        self.route("/new_game/<game_name>")(self.debug)
 
     def load_db_file(self):
         with open(DB_JSON_FILE) as fp:
             jsonificated = json.load(fp)
         return jsonificated
+
+    def overwrite_db(self, new_contents):
+        with open(DB_JSON_FILE, 'w') as wfp:
+            json.dump(new_contents, wfp)
+        return "success"
 
     def index(self):
         return "<h1>Spyfall backend</h1>"
@@ -32,6 +38,12 @@ class SpyfallApp(Flask):
 
     def dump_db(self):
         return jsonify(self.load_db_file())
+
+    def new_game(self, game_name):
+        db = self.load_db_file()
+        db['games'][game_name] = {}
+        self.overwrite_db(db)
+        return "Success: ",game_name
 
     def list_games(self):
         db_file = self.load_db_file()
